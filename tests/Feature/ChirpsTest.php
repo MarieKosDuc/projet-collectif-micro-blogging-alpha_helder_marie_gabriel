01b5ac9802c1d79a7db9d242d66a9e671636bc07
HelderF2821 <helder.ferreira2821@gmail.com>
2023-04-17 16:58:45 +0200

    new test

--- tests/Feature/ChirpsTest.php
+++ tests/Feature/ChirpsTest.php
@@ -2,16 +2,17 @@
 
 namespace Tests\Feature;
 
-use App\Models\Chirps;
+use App\Models\Chirp;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use SebastianBergmann\Type\VoidType;
 use Tests\TestCase;
 
 
 class ChirpsTest extends TestCase 
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     public function test_chirps_page_is_displayed(): void 
     {
@@ -22,5 +23,44 @@ class ChirpsTest extends TestCase
             ->get('/chirps');
         
         $response->assertOk();
+    }
+
+    public function test_chirps_can_be_posted(): void 
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->post('/chirps', [
+            "message" => "Hello World"
+        ]);           
+
+        $response
+            ->assertRedirect('/chirps');
+        
+        $this -> assertDatabaseHas('chirps', [
+            'user_id' => $user->id,
+            'message' => 'Hello World'
+        ]);
+    }
+
+    public function test_chirps_can_be_updated(): void
+    {
+        $user = User::factory()->create();
+        $chirp = Chirp::factory()->create();
+
+        $response = $this
+        ->actingAs($user)
+        ->post('/chirps', [
+            "message" => $chirp->message
+        ])     
+        ->put('/chirps', [
+            "message" => "Hello World"
+        ]);
+
+        $response
+            ->assertRedirect('/chirps');
+        
+        $this -> assertSame('Hello World', $chirp->message);
     }
 } 
